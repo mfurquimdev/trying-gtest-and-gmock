4073e47adc35a4697f670ef2864d4c5fd781094b
Mateus Furquim <furquim.axs@gmail.com>
2019-12-17 16:28:30 -0300

    Include a failed test and disable it

--- test/calculator_tests.cpp
+++ test/calculator_tests.cpp
@@ -11,3 +11,12 @@ TEST( CalculatorAdd, AddTwoIntegersReturnAnInteger )
 
 	ASSERT_EQ( 5, result );
 }
+
+TEST( CalculatorAdd, DISABLED_AddTwoNumbersReturnWrongResult )
+{
+	Calculator calc;
+
+	int result = calc.Add( 2, 2 );
+
+	ASSERT_EQ( 6, result );
+}
