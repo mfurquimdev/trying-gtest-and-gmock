8428a3134d45be7062bbe6ad46159c6e7208a528
Mateus Furquim <furquim.axs@gmail.com>
2019-12-18 10:17:12 -0300

    Add fixture class

--- test/stringAssertion_test.cpp
+++ test/stringAssertion_test.cpp
@@ -2,34 +2,42 @@
 
 #include <string>
 
-TEST( CStringsComparing, AssertStringEquals )
+class CStringComparissonFixture : public ::testing::Test
 {
-	const char* str1 = "abc";
-	const char* str2 = "abc";
-	const char* str3 = "cde";
-	const char* str4 = "cde";
+public:
+	const char* str1;
+	const char* str2;
+	const char* str3;
+	const char* str4;
 
+	virtual void SetUp()
+	{
+		str1 = static_cast< const char* > ("abc");
+		str2 = static_cast< const char* > ("abc");
+		str3 = static_cast< const char* > ("cde");
+		str4 = static_cast< const char* > ("CDE");
+	}
+
+	virtual void TearDown()
+	{
+	}
+};
+
+TEST_F( CStringComparissonFixture, AssertStringEquals )
+{
 	ASSERT_STREQ( str1, str2 );
-	EXPECT_STREQ( str3, str4 );
+	EXPECT_STREQ( str1, str2 );
 }
 
-TEST( CStringsComparing, AssertStringNotEquals )
+TEST_F( CStringComparissonFixture, AssertStringNotEquals )
 {
-	const char* str1 = "abc";
-	const char* str2 = "cde";
-	const char* str3 = "CDE";
-
-	ASSERT_STRNE( str1, str2 );
-	EXPECT_STRNE( str1, str3 );
+	ASSERT_STRNE( str1, str3 );
+	EXPECT_STRNE( str2, str4 );
 }
 
-TEST( CStringsComparing, AssertStringIgnoreCase )
+TEST_F( CStringComparissonFixture, AssertStringIgnoreCase )
 {
-	const char* str1 = "abc";
-	const char* str2 = "cde";
-	const char* str3 = "CDE";
-
-	ASSERT_STRCASENE( str1, str2 );
-	EXPECT_STRCASEEQ( str2, str3 );
+	ASSERT_STRCASENE( str1, str3 );
+	EXPECT_STRCASEEQ( str3, str4 );
 }
 
